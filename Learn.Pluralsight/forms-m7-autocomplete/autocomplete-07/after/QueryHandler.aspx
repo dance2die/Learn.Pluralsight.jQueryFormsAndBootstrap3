15a5083788ee1b614ab6a3c47e3e53c749c7afe4
dance2die <dontbothermewithspam@gmail.com>
2015-05-22 18:08:03 -0400

    Added project to learn about "Typeahead.js"

---
+++ Learn.Pluralsight/forms-m7-autocomplete/autocomplete-07/after/QueryHandler.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QueryHandler.aspx.cs" Inherits="forms_m7_autocomplete.autocomplete_07.before.QueryHandler" %>
---
+++ Learn.Pluralsight/forms-m7-autocomplete/autocomplete-07/after/QueryHandler.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace forms_m7_autocomplete.autocomplete_07.before
+{
+    public partial class QueryHandler : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> teams = new List<string>();
+            teams.Add("Giants");
+            teams.Add("Jets");
+
+            string q = Request.QueryString["q"];
+            var results = teams.Where(t => t.ToUpper().StartsWith(q.ToUpper()));
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            string output = jss.Serialize(results);
+            Response.Write(output);
+        }
+    }
+}
